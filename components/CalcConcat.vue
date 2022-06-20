99f6c20a273e6b34c022b2714fcbf0c5c537a8a4
shunprog <piassy.c0mp@gmail.com>
2022-06-20 12:16:20 +0900

    CalcConcatの追加

---
+++ components/CalcConcat.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>
+      文字列結合
+    </h3>
+    <p>computed</p>
+    <input v-model="str1" type="text"> + <input v-model="str2" type="text"> = {{ conCP }} <br>
+    <p>methods</p>
+    <input v-model="str3" type="text"> + <input v-model="str4" type="text"> = {{ calc('con', str3, str4) }} <br>
+  </div>
+</template>
+<script>
+import CalculateMixin from "~/mixins/CalculateMixin.js"
+
+export default {
+  mixins:[CalculateMixin]
+}
+</script>
+
